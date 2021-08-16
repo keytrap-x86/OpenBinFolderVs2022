70e73c25fda8b146c1c86326fde23b32b904ec99
varKeytrap <badniiw@gmail.com>
2021-08-16 15:03:14 +0200

    Ajoutez des fichiers projet.

---
+++ OpenBinFolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBinFolderVs2022
+{
+    using System;
+
+    /// <summary>
+    ///     Helper class that exposes all GUIDs used across VS Package.
+    /// </summary>
+    internal sealed partial class PackageGuids
+    {
+        public const string PackageGuidString = "645d738e-fdcc-4a60-826c-421b06cf1d71";
+        public static Guid OpenBinFolderPackageGuid = new Guid(PackageGuidString);
+
+        public const string PackageCommandSetGuidString = "9a55a2b4-3e29-4359-882b-fa5f51c09300";
+        public static Guid OpenBinFolderCommandSetGuid = new Guid(PackageCommandSetGuidString);
+    }
+
+    /// <summary>
+    ///     Helper class that encapsulates all CommandIDs uses across VS Package.
+    /// </summary>
+    internal sealed partial class PackageIds
+    {
+        public const int CmdOpenBinFolder = 0xACAB;
+    }
+}
---
+++ OpenBinFolderPackage.cs
@@ -0,0 +1,93 @@
+﻿
+using EnvDTE;
+
+using EnvDTE80;
+
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+
+using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace OpenBinFolderVs2022
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(PackageGuids.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class OpenBinFolderPackage : AsyncPackage
+    {
+        /// <summary>
+        /// OpenBinFolderVs2022Package GUID string.
+        /// </summary>
+
+
+        public static DTE2 _dte;
+
+        #region Package Members
+
+        protected async override System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            _dte = await GetServiceAsync(typeof(DTE)) as DTE2;
+
+            Assumes.Present(_dte);
+
+            if (await GetServiceAsync(typeof(IMenuCommandService)) is OleMenuCommandService mcs)
+            {
+                CommandID menuCommandID = new CommandID(PackageGuids.OpenBinFolderCommandSetGuid, PackageIds.CmdOpenBinFolder);
+                OleMenuCommand menuItem = new OleMenuCommand(OpenBinFolderWithFileExplorer, menuCommandID);
+                mcs.AddCommand(menuItem);
+            }
+
+        }
+
+        #endregion
+
+
+        private void OpenBinFolderWithFileExplorer(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Find active projects
+            var activeProjects = (Array)_dte.ActiveSolutionProjects;
+
+            foreach (var (currentProjectPath, currentProjectBinPath) in
+            from Project activeProject in activeProjects
+            let currentProjectPath = Path.GetDirectoryName(activeProject.FullName)
+            let currentProjectOutputPath = activeProject.ConfigurationManager.ActiveConfiguration.Properties.Item("OutputPath").Value.ToString()
+            let currentProjectBinPath = Path.Combine(currentProjectPath, currentProjectOutputPath)
+            select (currentProjectPath, currentProjectBinPath))
+            {
+                if (Directory.Exists(currentProjectBinPath))
+                    System.Diagnostics.Process.Start(currentProjectBinPath);
+                else
+                    System.Diagnostics.Process.Start(currentProjectPath);
+            }
+        }
+    }
+}
